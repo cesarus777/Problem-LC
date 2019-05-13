77923f0e44a448f8bbbd9e6439498363cec81ae9
cesarus777 <ulius.cesarus@gmail.com>
2019-05-13 17:33:34 +0300

    Update

--- HashTable.c
+++ HashTable.c
@@ -1,10 +1,14 @@
+#include "stdlib.h"
+
 #include "HashTable.h"
 
-struct hash_table_t *create_hash_table(int capacity)
+struct hash_table_t create_hash_table(int capacity)
 {
+  struct hash_table_t res = { 0, NULL };
   if(capacity == 0)
-    return NULL;
-  struct hash_table_t *res = (struct hash_table_t *) calloc(capacity, sizeof(struct node_t));
+    return res;
+  res.capacity = capacity;
+  res.elems = (struct node_t **) calloc(capacity, sizeof(struct node_t));
   return res;
 }
 
@@ -22,4 +26,27 @@ struct node_t *add_elem(struct node_t *elem, struct hash_table_t *table)
   return elem;
 }
 
-struct node_t *find_elem();
+struct node_t *find_elem(struct node_t *elem, struct hash_table_t *table)
+{
+  if((elem == NULL) || (table == NULL))
+    return NULL;
+  int elem_hash = find_hash(elem->data.index);
+  if(table->elems[elem_hash] == elem)
+    return table->elems[elem_hash];
+  return NULL;
+}
+
+void pop_elem(struct node_t *elem, struct hash_table_t *table)
+{
+  if((elem == NULL) || (table == NULL))
+    return;
+  int elem_hash = find_hash(elem->data.index);
+  if(table->elems[elem_hash] == elem)
+    table->elems[elem_hash] = NULL;
+}
+
+void destroy_hash_table(struct hash_table_t *table)
+{
+  table->capacity = 0;
+  free(table->elems);
+}
--- HashTable.h
+++ HashTable.h
@@ -13,16 +13,16 @@ enum { HASH_MAX_SIZE = 100 };
 int find_hash(int index);
 
 //creation hash table
-struct hash_table_t *create_hash_table(int capacity);
+struct hash_table_t create_hash_table(int capacity);
 
 //find elem in table
-struct node_t *find_elem(int index, struct hash_table_t *table);
+struct node_t *find_elem(struct node_t *elem, struct hash_table_t *table);
 
 //add elem in table
 struct node_t *add_elem(struct node_t *elem, struct hash_table_t *table);
 
 //pop elem from table
-void pop_elem(struct node_t *elem);
+void pop_elem(struct node_t *elem, struct hash_table_t *table);
 
 //destroy table
-void destroy_table(struct hash_table_t *hash_table);
+void destroy_hash_table(struct hash_table_t *table);
