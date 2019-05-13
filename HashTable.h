ea4fa8378c4b395d15fb9de7ec839111bdc495e1
cesarus777 <ulius.cesarus@gmail.com>
2019-05-13 16:47:20 +0300

    Update

--- HashTable.h
+++ HashTable.h
@@ -10,13 +10,13 @@ struct hash_table_t {
 struct hash_table_t create_hash_table(int capacity);
 
 //find elem in table
-struct *page find_elem(int index);
+struct node_t *find_elem(int index);
 
 //add elem in table
-struct *page add_elem(struct *page elem);
+struct node_t *add_elem(struct node_t *elem);
 
 //pop elem from table
-void pop_elem(struct *page elem);
+void pop_elem(struct node_t *elem);
 
 //destroy table
-void destroy_table(struct *hash_table_t hash_table);
+void destroy_table(struct hash_table_t *hash_table);
